653fbb608e78370749f4e6547d2ec429d7b3585a
leenrichards <leenrichards@yahoo.com>
2022-05-01 09:18:47 -0700

    added header and logo

--- src/App.js
+++ src/App.js
@@ -126,7 +126,7 @@ class App extends Component {
     return (
       <div className="App">
 
-        <div className="searchboxes">
+        <div className="SearchBoxes">
           <CitySearch
             locations={locations}
             updateEvents={this.updateEvents}
